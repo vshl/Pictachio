891ea9eae2b68159b8a36a1b617511a4842dccbf
Vishal Ravi Shankar <vshankar@mail.sfsu.edu>
2015-10-29 20:11:45 -0700

    Added new activity DetailActivity

---
+++ app/src/main/java/edu/sfsu/csc780/pictachio/DetailActivity.java
@@ -0,0 +1,14 @@
+package edu.sfsu.csc780.pictachio;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DetailActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+
+}
