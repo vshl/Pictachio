77d727f10b55bc9d64dfef0a362c6f66073dc340
Vishal Ravi Shankar <vshankar@mail.sfsu.edu>
2015-11-27 11:42:47 -0800

    Implemented pan, zoom of image with DeepZoom. Made detail activity fullscreen.

--- app/src/main/java/edu/sfsu/csc780/pictachio/DetailActivity.java
+++ app/src/main/java/edu/sfsu/csc780/pictachio/DetailActivity.java
@@ -1,33 +1,26 @@
 package edu.sfsu.csc780.pictachio;
 
 import android.app.Activity;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
-import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.koushikdutta.ion.Ion;
+
+import uk.co.senab.photoview.PhotoView;
 
 public class DetailActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
+        PhotoView photoView = new PhotoView(this);
+        photoView.setMaximumScale(16);
+        setContentView(photoView);
         Uri uri = Uri.parse(getIntent().getStringExtra("image"));
-        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-        Bitmap bitmap = BitmapFactory.decodeFile(uri.getPath(), bmOptions);
-        bitmap = Bitmap.createScaledBitmap(bitmap, bitmap.getWidth(), bitmap.getHeight(), true);
-        Toast.makeText(this.getBaseContext(), uri.getPath(), Toast.LENGTH_SHORT).show();
-        ImageView iv = (ImageView) findViewById(R.id.imageDetail);
-        int w = (int) (bitmap.getWidth() * 0.5);
-        int h = (int) (bitmap.getHeight() * 0.5);
-        Ion.with(iv)
-                .resize(w, h)
-                .fitXY()
-                .centerCrop()
+        Ion.with(photoView)
+                .fitCenter()
+                .smartSize(true)
+                .deepZoom()
                 .load(uri.getPath());
     }
 
