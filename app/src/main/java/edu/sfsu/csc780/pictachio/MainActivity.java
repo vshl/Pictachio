450632db5e07b0b2454d6c22c63baf4e3f188cb3
Vishal Ravi Shankar <vshankar@mail.sfsu.edu>
2015-11-27 17:40:32 -0800

    Rename of action_refresh id

--- app/src/main/java/edu/sfsu/csc780/pictachio/MainActivity.java
+++ app/src/main/java/edu/sfsu/csc780/pictachio/MainActivity.java
@@ -160,7 +160,7 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_refresh) {
             CameraRollFragment cameraRollFragment = (CameraRollFragment)
                     getFragmentManager().findFragmentById(R.id.fragment_container);
             if (cameraRollFragment != null)
