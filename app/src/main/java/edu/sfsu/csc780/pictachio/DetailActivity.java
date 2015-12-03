f01c0930799985414bf83074231057846d9a5da2
Vishal Ravi Shankar <vshankar@mail.sfsu.edu>
2015-12-03 11:28:01 -0800

    Enabled video playback for videos.

--- app/src/main/java/edu/sfsu/csc780/pictachio/DetailActivity.java
+++ app/src/main/java/edu/sfsu/csc780/pictachio/DetailActivity.java
@@ -3,6 +3,8 @@ package edu.sfsu.csc780.pictachio;
 import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 import com.koushikdutta.ion.Ion;
 
@@ -13,15 +15,24 @@ public class DetailActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        PhotoView photoView = new PhotoView(this);
-        photoView.setMaximumScale(16);
-        setContentView(photoView);
         Uri uri = Uri.parse(getIntent().getStringExtra("image"));
-        Ion.with(photoView)
-                .fitCenter()
-                .smartSize(true)
-                .deepZoom()
-                .load(uri.getPath());
+        if (uri.toString().endsWith(".mp4")) {
+            setContentView(R.layout.activity_detail);
+            VideoView videoView = (VideoView) findViewById(R.id.video_view);
+            videoView.setVideoURI(uri);
+            videoView.setMediaController(new MediaController(this));
+            videoView.requestFocus();
+            videoView.start();
+        } else {
+            PhotoView photoView = new PhotoView(this);
+            photoView.setMaximumScale(16);
+            setContentView(photoView);
+            Ion.with(photoView)
+                    .fitCenter()
+                    .smartSize(true)
+                    .deepZoom()
+                    .load(uri.getPath());
+        }
     }
 
 }
