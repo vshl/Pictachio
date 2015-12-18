005bf39518f5b2130f37345fbb99f479db23d47f
Vishal Ravi Shankar <vshankar@mail.sfsu.edu>
2015-12-18 02:37:43 -0800

    Moved camera code back to MainActivity.

--- app/src/main/java/edu/sfsu/csc780/pictachio/activities/MainActivity.java
+++ app/src/main/java/edu/sfsu/csc780/pictachio/activities/MainActivity.java
@@ -6,7 +6,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
@@ -16,8 +18,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import edu.sfsu.csc780.pictachio.R;
 import edu.sfsu.csc780.pictachio.fragments.AllPhotosFragment;
@@ -27,10 +36,18 @@ import edu.sfsu.csc780.pictachio.fragments.VideosFragment;
 public class MainActivity extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
-    LocalCamera lc = new LocalCamera(this);
-    private Toolbar toolbar;
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle drawerToggle;
+    private String mCurrentPhotoPath;
+    Button.OnClickListener mTakePhotoOnClickListener =
+            new Button.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (isExternalStorageWritable()) {
+                        dispatchTakePictureIntent();
+                    }
+                }
+            };
 
     /**
      * Indicates whether the specified action can be used as an intent. This
@@ -53,13 +70,18 @@ public class MainActivity extends AppCompatActivity {
         return list.size() > 0;
     }
 
+    private static boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        return Environment.MEDIA_MOUNTED.equals(state);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // Instantiate toolbar
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         // Obtain DrawerLayout
@@ -91,7 +113,7 @@ public class MainActivity extends AppCompatActivity {
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         setBtnListenerOrDisable(
                 fab,
-                lc.mTakePhotoOnClickListener,
+                mTakePhotoOnClickListener,
                 MediaStore.ACTION_IMAGE_CAPTURE
         );
     }
@@ -141,7 +163,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            lc.handleCameraPhoto();
+            handleCameraPhoto();
         }
     }
 
@@ -178,6 +200,62 @@ public class MainActivity extends AppCompatActivity {
         drawerToggle.syncState();
     }
 
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss",
+                Locale.getDefault()).format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storagePath = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DCIM);
+        File storageDir = new File(storagePath.getAbsolutePath(), "/Camera");
+
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir     /* directory */
+        );
+        // Save a file: path for use with ACTION_VIEW  intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure there is a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+                photoFile = null;
+            }
+
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                        Uri.fromFile(photoFile));
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(mCurrentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        sendBroadcast(mediaScanIntent);
+    }
+
+    public void handleCameraPhoto() {
+        if (mCurrentPhotoPath != null) {
+            galleryAddPic();
+            mCurrentPhotoPath = null;
+        }
+    }
+
     private void setBtnListenerOrDisable(
             FloatingActionButton btn,
             FloatingActionButton.OnClickListener onClickListener,
@@ -190,3 +268,4 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 }
+
