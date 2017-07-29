2902ebceac7408827a00b28356daa213f4378d31
Vishal Ravi Shankar <rsh.vishal@gmail.com>
2017-07-29 16:18:56 -0700

    added permission request for api-m and above

--- app/src/main/java/me/vshl/apps/pictachio/fragments/CameraRollFragment.java
+++ app/src/main/java/me/vshl/apps/pictachio/fragments/CameraRollFragment.java
@@ -1,14 +1,19 @@
 package me.vshl.apps.pictachio.fragments;
 
 
+import android.Manifest;
 import android.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -30,8 +35,10 @@ import me.vshl.apps.pictachio.activities.DetailActivity;
  */
 public class CameraRollFragment extends Fragment {
 
+    private static final int REQUEST_READ_EXTERNAL_STORAGE = 1;
     private final ContentAdapter adapter = new ContentAdapter();
     private ArrayList<String> imageList = new ArrayList<>();
+    private View mLayout;
 
     public CameraRollFragment() {
         // Required empty public constructor
@@ -40,6 +47,11 @@ public class CameraRollFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mLayout = getActivity().findViewById(R.id.drawer_layout);
+        if (ActivityCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestReadExternalStoragePermission();
+        }
         updateAdapter();
     }
 
@@ -77,6 +89,43 @@ public class CameraRollFragment extends Fragment {
     @SuppressWarnings("unchecked")
     public void updateAdapter() {
         new LoadImages().execute(imageList);
+    }
+
+    private void requestReadExternalStoragePermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+                Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            Snackbar.make(mLayout, R.string.permission_read_external_storage_rationale,
+                    Snackbar.LENGTH_INDEFINITE)
+                    .setAction(R.string.ok, new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            ActivityCompat.requestPermissions(getActivity(),
+                                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                                    REQUEST_READ_EXTERNAL_STORAGE);
+                        }
+                    })
+                    .show();
+        } else {
+            ActivityCompat.requestPermissions(getActivity(),
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    REQUEST_READ_EXTERNAL_STORAGE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_READ_EXTERNAL_STORAGE) {
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Snackbar.make(mLayout, R.string.permission_available_read_external_storage,
+                        Snackbar.LENGTH_SHORT).show();
+            } else {
+                Snackbar.make(mLayout, R.string.permission_not_granted, Snackbar.LENGTH_SHORT)
+                        .show();
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
     }
 
     /**
